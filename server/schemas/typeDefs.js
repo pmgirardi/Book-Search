c3390412f55c21ba898a0e1d574c5a743f16c7b8
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-15 16:45:25 -0700

    added typeDefs.js and defined the necessary Query and Mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,62 @@
 const { gql } = require('apollo-server-express');
 
+/* `	* `typeDefs.js`: Define the necessary `Query` and `Mutation` types:
+
+		* `Query` type:
+
+			* `me`: Which returns a `User` type.
+		
+		* `Mutation` type:
+
+			* `login`: Accepts an email and password as parameters; returns an `Auth` type.
+
+			* `addUser`: Accepts a username, email, and password as parameters; returns an `Auth` type.
+
+			* `saveBook`: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a `User` type. (Look into creating what's known as an `input` type to handle all of these parameters!)
+
+			* `removeBook`: Accepts a book's `bookId` as a parameter; returns a `User` type. */
+
 const typeDefs = gql`
-  type Tech {
+  type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    me: User
   }
 
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+  
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
