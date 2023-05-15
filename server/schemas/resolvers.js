c425b4c49f2cc36111e5fd7b8816431539dd0ffe
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-15 16:35:14 -0700

    added schemas directory, index.js with exported typeDefs and resolvers, resolvers.js defined query and mutation functionality to work with mongoose models using the functionality in the 'user-controller.js' file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,29 +1,73 @@
-const { Tech, Matchup } = require('../models');
+// Define the query and mutation functionality to work with the Mongoose models.
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
+// **Hint**: Use the functionality in the `user-controller.js` as a guide.
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Tech.find({});
-    },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return Matchup.find(params);
-    },
-  },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id })
+    }
+    throw new AuthenticationError('You are not logged in');
+  }
+},
+ 
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+// Add user
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Email address not found');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return user;
+      }
+  
+      throw new AuthenticationError('You must be logged in');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return user;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
-};
+}
+}
+
 
 module.exports = resolvers;
