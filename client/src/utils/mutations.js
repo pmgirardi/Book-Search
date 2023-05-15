bc6a3d56d182a656fb5e2e1e6e13fce2ce325eb4
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-15 16:53:49 -0700

    added queries and mutation files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client';
+
+/* `mutations.js`:
+
+	* `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+
+	* `ADD_USER` will execute the `addUser` mutation.
+
+	* `SAVE_BOOK` will execute the `saveBook` mutation.
+
+	* `REMOVE_BOOK` will execute the `removeBook` mutation.
+  */
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+// `queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
