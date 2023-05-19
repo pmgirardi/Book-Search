2870c7a11aaed8beefa1c53d1f03fffc54847666
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-18 18:42:35 -0700

    refactored resolvers and auth code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,15 +8,13 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        if (context.user) {
-          return User.findOne({ _id: context.user._id })
-    }
-    throw new AuthenticationError('You are not logged in');
-  }
-},
- 
+        return await User.findOne({ _id: context.user._id })
+      }
+      throw new AuthenticationError('You are not logged in');
+    },
+  },
   Mutation: {
-// Add user
+    // Add user
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
@@ -49,7 +47,7 @@ const resolvers = {
 
         return user;
       }
-  
+
       throw new AuthenticationError('You must be logged in');
     },
     removeBook: async (parent, { bookId }, context) => {
@@ -66,8 +64,7 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
   },
-}
-}
+};
 
 
 module.exports = resolvers;
